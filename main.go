ff514b87b37ea2c05f4fad9e9003fc1ad9c1db0b
Kunal Kushwaha <kunal.kushwaha@gmail.com>
2015-07-29 10:42:00 +0900

    Image Walk with memoization technique
    Signed-off-by: Kunal Kushwaha <kunal.kushwaha@gmail.com>

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ type ImageInfo struct {
 	parent        string
 	actualSize    int64
 	totalSize     int64
-	tag	[]string
+	tag           []string
 	refrenceCount int
 }
 
@@ -60,46 +60,98 @@ type ImageInfoNode struct {
 
 func (dcl *dclient) getImageDiskUsage(image string) {
 	fmt.Println(image)
-	imageList, err := dcl.client.ListImages(true)
+	imageList, err := dcl.client.ListImages(false)
 	if err != nil {
 		fmt.Println("Error while fetching image list")
 		return
 	}
 
-	imageMap := make(map[string]ImageInfoNode)
-
+	imageMap := make(map[string]*ImageInfoNode)
+	count := 0
 	for _, image := range imageList {
-		fmt.Println(image.Id, image.Size/1048576, "MB", image.VirtualSize/1048576)
-		imageMap[image.Id] = dcl.getImageTree(image, imageMap)
+		temp, _ := dcl.client.InspectImage(image.Id)
+		imageMap[image.Id] = dcl.getImageTree(temp, imageMap)
+		imageMap[image.Id].imageInfo.tag = image.RepoTags
 	}
-
-	fmt.Println("\n", imageMap)
+	count = 0
+	for k, _ := range imageMap {
+		dumpImageTree(imageMap[k], 0)
+		fmt.Println("---------------------------")
+		count++
+	}
+	fmt.Println(count)
 
 }
 
-func (dcl *dclient) getImageTree(image *dockerclient.Image, imageMap map[string]ImageInfoNode) *ImageInfoNode {
+func dumpImageTree(image *ImageInfoNode, treeDepth int) bool {
+	if image.parent == nil {
+		return false
+	}
+	//for i := 0; i < treeDepth; i++ {
+	if treeDepth > 0 {
+		fmt.Printf("--")
+	}
+	fmt.Printf("%-20s %32s %6d MB %6d MB\n", image.imageInfo.tag, image.imageInfo.parent, image.imageInfo.actualSize/(1024*1024), image.imageInfo.totalSize/(1024*1024))
+	return dumpImageTree(image.parent, treeDepth+1)
 
-	if image.ParentId == "" {
-		return ImageInfoNode{parent:nil}
+}
+
+func (dcl *dclient) getImageTree(image *dockerclient.ImageInfo, imageMap map[string]*ImageInfoNode) *ImageInfoNode {
+
+	if image.Parent == "" {
+		return &ImageInfoNode{parent: nil}
 	}
 
-	if image.ParentId != "" {
+	if image.Parent != "" {
 
-		//Find the Node in map.
-
+		//	fmt.Println(image.Id)
 		//If not found in map, add details.
-		var parentNode ImageInfoNode
-		parentNode.imageInfo.parent = image.ParentId
+		parentNode := new(ImageInfoNode)
+		parentNode.imageInfo.parent = image.Parent
 		parentNode.imageInfo.actualSize = image.Size
 		parentNode.imageInfo.totalSize = image.VirtualSize
-		parentNode.imageInfo.tag = image.RepoTags
+		//		parentNode.imageInfo.tag = image.RepoTags
 		//TODO: fix later
 		parentNode.imageInfo.refrenceCount = 0
 
-		imageMap[image.Id] = parentNode
-		parentImage,_ := dcl.client.InspectImage(image.Id)
+		//Find the Node in map.
+		if imageMap[image.Id] != nil {
+			return imageMap[image.Id]
+		}
+
+		foundNode, res := findImageTree(image.Id, imageMap)
+		if res {
+			return foundNode
+		}
+		parentImage, _ := dcl.client.InspectImage(image.Parent)
 		parentNode.parent = dcl.getImageTree(parentImage, imageMap)
-		return &parentNode
+		//fmt.Println("Return..", parentNode.imageInfo.parent)
+		return parentNode
 	}
-		return ImageInfoNode{parent:nil}
+	return &ImageInfoNode{parent: nil}
+}
+
+func findImageTree(imageId string, imageMap map[string]*ImageInfoNode) (*ImageInfoNode, bool) {
+
+	//Loop in map,
+	// and traverse each list of map item, using ImageWalk.
+	for _, v := range imageMap {
+		image, res := ImageWalk(imageId, v)
+		if res {
+			return image, res
+		}
+	}
+	return &ImageInfoNode{parent: nil}, false
+
+}
+
+func ImageWalk(imageId string, image *ImageInfoNode) (*ImageInfoNode, bool) {
+	if image.parent == nil {
+		return &ImageInfoNode{parent: nil}, false
+	}
+	if image.imageInfo.parent == imageId {
+		return image.parent, true
+	}
+	return ImageWalk(imageId, image.parent)
+
 }
