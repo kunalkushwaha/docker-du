1b845aaf9300af4a3496b764473166ee6ac3d072
Kunal Kushwaha <kunal.kushwaha@gmail.com>
2015-08-27 18:35:53 +0900

    Code fixed for nested tree
    Signed-off-by: Kunal Kushwaha <kunal.kushwaha@gmail.com>

--- main.go
+++ main.go
@@ -72,13 +72,13 @@ func printImageTree(imageTree []ImageInfoList, tab int) {
 }
 
 func printSubTree(tree ImageInfoList, tab int) {
-	for i := 0; i < tab; i++ {
-		fmt.Printf("_")
-	}
-	fmt.Printf("_ %20s %14s  %d MB, %d\n", tree.imageInfo.tag, tree.imageInfo.id[0:12], tree.imageInfo.actualSize/(1024*1024), len(tree.child))
+	//for i := 0; i < tab/2; i++ {
+	//	fmt.Printf("_")
+	//}
+	fmt.Printf("_  %14s  %d MB, %d\n", tree.imageInfo.id[0:12], tree.imageInfo.actualSize/(1024*1024), len(tree.child))
 	for _, image := range tree.child {
 		for i := 0; i < tab; i++ {
-			fmt.Printf(" ")
+			fmt.Printf("  ")
 		}
 		fmt.Printf("|_")
 		printSubTree(image, tab+1)
@@ -96,12 +96,18 @@ func (dcl *dclient) buildImageTree(image string) {
 	var imageTree []ImageInfoList
 
 	for _, image := range imageList {
+		//if image.Id != "05b0290b4b5e63acba5007ee016d5de2142eeacb1400a65bffdaf01c6c3652d1" {
+		if image.Id != "d2a0ecffe6fa4ef3de9646a75cc629bbd9da7eead7f767cb810f9808d6b3ecb6" &&
+		image.Id != "05b0290b4b5e63acba5007ee016d5de2142eeacb1400a65bffdaf01c6c3652d1"{
+			continue
+		}
 		temp, _ := dcl.client.InspectImage(image.Id)
 		tree := dcl.buildImageTreeDetails(temp, imageTree, image.RepoTags[0])
 		imageTree = tree
 
 	}
 	fmt.Println("-------------------- Final Tree ---------------")
+	//fmt.Println(imageTree)
 	printImageTree(imageTree, 0)
 }
 
@@ -123,10 +129,10 @@ func (dcl *dclient) buildImageTreeDetails(image *dc.ImageInfo, imageTree []Image
 	}
 	foundNode := checkRootExist(node.(*dc.ImageInfo), imageTree)
 	if foundNode < 0 {
-		//		fmt.Println("--- Node not found in Tree ---")
+		fmt.Println("--- Node not found in Tree ---")
 		imageTree = addNodeToImageTree(imageTree, node.(*dc.ImageInfo), s)
 	} else {
-		//		fmt.Println("--- Node found in Tree ---")
+		fmt.Println("--- Node found in Tree ---")
 		imageTree = addToBranchNode(foundNode, s, imageTree)
 	}
 	return imageTree
@@ -148,14 +154,26 @@ func addNodeToImageTree(imageTree []ImageInfoList, image *dc.ImageInfo, s *Stack
 func addNodesAsChild(image ImageInfoList, s *Stack) ImageInfoList {
 
 	tempParent := s.Pop()
-
-	for tempParent != nil {
+	if tempParent != nil {
 		temp := tempParent.(*dc.ImageInfo)
+	//	fmt.Println(temp.Id)
 		node := ImageInfoList{imageToNode(temp), nil}
+		node = addNodesAsChild(node, s)
 		image.child = append(image.child, node)
-		image = node
-		tempParent = s.Pop()
+	}
 
+	return image
+}
+
+func addNodesAsChildQueue(image ImageInfoList, s *Stack) ImageInfoList {
+
+	tempParent := s.Dequeue()
+	if tempParent != nil {
+		temp := tempParent.(*dc.ImageInfo)
+	//	fmt.Println(temp.Id)
+		node := ImageInfoList{imageToNode(temp), nil}
+		node = addNodesAsChildQueue(node, s)
+		image.child = append(image.child, node)
 	}
 
 	return image
@@ -173,6 +191,9 @@ func checkRootExist(node *dc.ImageInfo, imageTree []ImageInfoList) int {
 
 func addToBranchNode(nodeIdx int, s *Stack, imageTree []ImageInfoList) []ImageInfoList {
 
+	//Till depth of tree
+	//	branchTree := imageTree
+	//	for tree := range branchTree[nodeIdx:] {
 	for i, tempNode := range imageTree[nodeIdx:] {
 		stackNode := s.Peek().(*dc.ImageInfo)
 
@@ -180,7 +201,11 @@ func addToBranchNode(nodeIdx int, s *Stack, imageTree []ImageInfoList) []ImageIn
 			// Access to prev node in imageTree
 			// Add node a +1 child of that node.
 			prevNode := imageTree[i]
-			node := addNodesAsChild(prevNode, s)
+			// Before adding, pop from stack, and create a list and add them as list.
+			queue := NewStack()
+
+		
+			node := addNodesAsChildQueue(prevNode, s)
 			prevNode.child = append(prevNode.child, node)
 			imageTree[i] = prevNode
 
@@ -191,6 +216,8 @@ func addToBranchNode(nodeIdx int, s *Stack, imageTree []ImageInfoList) []ImageIn
 		}
 		stackNode = s.Pop().(*dc.ImageInfo)
 	}
+	fmt.Println("== Printing intertnally == ")
+	printImageTree(imageTree, 0)
 	return imageTree
 
 }
